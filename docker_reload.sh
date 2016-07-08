4550b99eaf629db7da395573d66220ae01f5a391
Jonathan <jonathan.pastor@me.com>
2016-07-08 16:05:32 -0500

    remove triggering of an error if docker and docker-compose are already installed;

--- docker_reload.sh
+++ docker_reload.sh
@@ -12,14 +12,14 @@ fi
 # Check if APT or yum are installed
 APT_PATH=$(which apt-get)
 YUM_PATH=$(which yum)
-if [ "${APT_PATH}${YUM_PATH}" == "" ]; then
-    echo "Could  not find APT nor yum.  Are you running this  script from a debian or RHEL like system?"
-    exit 1
-fi
 
 # Check if docker is installed
 DOCKER_PATH=$(which docker)
 if [ "$DOCKER_PATH" == "" ]; then
+    if [ "${APT_PATH}${YUM_PATH}" == "" ]; then
+        echo "Could  not find APT nor yum.  Are you running this  script from a debian or RHEL like system?"
+        exit 1
+    fi
     # Install docker
     curl -fsSL https://test.docker.com/ | sh
 fi
@@ -30,6 +30,10 @@ sudo service docker start
 # Check if docker-compose is installed
 DOCKER_COMPOSE_PATH=$(which docker-compose)
 if [ "$DOCKER_COMPOSE_PATH" == "" ]; then
+    if [ "${APT_PATH}${YUM_PATH}" == "" ]; then
+        echo "Could  not find APT nor yum.  Are you running this  script from a debian or RHEL like system?"
+        exit 1
+    fi
     # Install docker-compose
     curl -L https://github.com/docker/compose/releases/download/1.8.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
